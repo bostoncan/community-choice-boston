65e4f90f304f5c2c0ed8fbb3db49cbb367b68cf1
Greg Thole <gthole@gmail.com>
2017-01-03 16:27:55 -0500

    Add parallax effect on header

--- src/js/views/home/index.js
+++ src/js/views/home/index.js
@@ -6,13 +6,27 @@ var Backbone = require('backbone-lodash'),
     MapView = require('./components/map');
 
 module.exports = Backbone.View.extend({
-    initialize(options) {
+    initialize: function(options) {
+        // Set the header image to have a nice parallax mode
+        var bg = $('.bg'),
+            jumboHeight = $('.jumbotron').outerHeight();
+
+        function parallax(){
+            var scrolled = $(window).scrollTop();
+            bg.css('height', (jumboHeight - scrolled) + 'px');
+        }
+        $(window).scroll(parallax);
+        $('.jumbotron').css('background', 'transparent');
+        parallax();
+
+        // Clicking the button moves you to the form
         $("#sign").click(function() {
             $('html,body').animate({
                 scrollTop: $("form").offset().top
             }, 'slow');
         });
 
+        // Sub views
         new CalenderView({
             el: '#next-event',
             id: options.config.CAL_ID,
