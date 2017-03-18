efa4814fc28fc9f21d37f96ce655812814706e84
Greg Thole <gthole@gmail.com>
2017-03-18 12:29:30 -0400

    Update map

--- webpack/src/js/views/home/components/map.js
+++ webpack/src/js/views/home/components/map.js
@@ -4,6 +4,62 @@ var d3 = require('d3'),
     topojson = require('topojson'),
     $ = require('jquery');
 
+var ccaTowns = {
+    'ACTON': 4,
+    'ARLINGTON': 3,
+    'ASHBY': 1,
+    'AQUINNAH': 1,
+    'BOSTON': 4,
+    'BOURNE': 1,
+    'BREWSTER': 1,
+    'BROOKLINE': 3,
+    'CAMBRIDGE': 4,
+    'CHATHAM': 1,
+    'CHILMARK': 1,
+    'CLARKSBURG': 1,
+    'DALTON': 1,
+    'DEDHAM': 2,
+    'DENNIS': 1,
+    'EASTHAM': 1,
+    'EDGARTOWN': 1,
+    'FALMOUTH': 1,
+    'FLORIDA': 1,
+    'GLOUCESTER': 3,
+    'GREENFIELD': 1,
+    'HAMILTON': 3,
+    'HARWICH': 1,
+    'LANCASTER': 1,
+    'LANESBOROUGH': 1,
+    'LENOX': 1,
+    'LEXINGTON': 4,
+    'LOWELL': 1,
+    'LUNENBURG': 1,
+    'MARLBOROUGH': 1,
+    'MASHPEE': 1,
+    'MELROSE': 2,
+    'NATICK': 1,
+    'NEW MARLBOROUGH': 1,
+    'NORTH ADAMS': 1,
+    'OAK BLUFFS': 1,
+    'ORLEANS': 1,
+    'PROVINCETOWN': 1,
+    'SANDWICH': 1,
+    'SALEM': 2,
+    'SHEFFIELD': 1,
+    'SOMERVILLE': 3,
+    'SUDBURY': 3,
+    'SWAMPSCOTT': 2,
+    'TISBURY': 1,
+    'TRURO': 1,
+    'TYRINGHAM': 1,
+    'WELLFLEET': 1,
+    'WEST STOCKBRIDGE': 1,
+    'WEST TISBURY': 1,
+    'WILLIAMSTOWN': 1,
+    'WINCHESTER': 3,
+    'YARMOUTH': 1
+};
+
 function titleCase(name) {
     return name
         .split(' ')
@@ -23,14 +79,7 @@ module.exports = function() {
 
     // Convert the topojson to geojson
     var geojson = topojson.feature(topology, topology.objects.towns),
-        path = d3.geo.path().projection(null),
-        townLookup = {},
-        defaultTownContext = {
-            ccaEstablished: 'Not yet',
-            type: 0,
-            ccaName: '-',
-            renewables: '-'
-        };
+        path = d3.geo.path().projection(null);
 
     d3.select('#map')
         .append('svg').attr({width: 615, height: 375})
@@ -42,21 +91,28 @@ module.exports = function() {
         .append('path')
         .attr({
             'class': function(d) {
-                var cca = d.properties.cca || defaultTownContext;
-                switch(cca.type) {
+                var cca = ccaTowns[d.properties.town];
+                switch(cca) {
                     case 1:
                         return 'town town-green';
                     case 2:
+                        return 'town town-dark-green';
+                    case 3:
                         return 'town town-orange';
+                    case 4:
+                        return 'town town-gray';
                     default:
                         return 'town';
                 }
             },
             'data-town': function(d) {
-                townLookup[d.properties.town] = d.properties.cca;
                 return d.properties.town;
             },
             d: path
+        })
+        .append('svg:title')
+        .text(function(d) {
+            return titleCase(d.properties.town);
         });
         $('.town').hover(function(el) {
             var town = $(el.currentTarget).data('town');
@@ -68,14 +124,26 @@ module.exports = function() {
         /*
          * Map legend
          */
-        var legendText = ['Participating in a CCE', 'Proposed', 'None yet'],
-            color = d3.scale.ordinal().range(['#fff', 'orange', 'green']);
-        color.domain([0,1,2]); // setting the range of the input data
+        var legendText = [
+            'Participating in a CCE with extra renewables',
+            'Participating in a CCE',
+            'Planning a CCE',
+            'Proposed'
+        ];
+
+        var color = d3.scale.ordinal().range([
+            '#cfcfcf',
+            'orange',
+            '#8BB381',
+            'green'
+        ]);
+
+        color.domain([0,1,2,3]); // setting the range of the input data
 
         var legend = d3.select('#legend').append('svg')
             .attr('class', 'legend')
-            .attr('width', 180)
-            .attr('height', 60)
+            .attr('width', 320)
+            .attr('height', 80)
             .selectAll('g')
             .data(color.domain().slice().reverse())
             .enter()
