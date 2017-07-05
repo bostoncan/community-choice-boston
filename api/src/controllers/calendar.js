522925c31bc9757937b6dc41d1bfa9e8367c03d2
Greg Thole <gthole@gmail.com>
2017-07-05 08:09:25 -0400

    Use Eventbrite instead of Google Calendar for events

--- api/src/controllers/calendar.js
+++ api/src/controllers/calendar.js
@@ -1,62 +1,47 @@
 'use strict';
 
-const https = require('https'),
-      qs = require('querystring');
-
-const FIELDS = ['start', 'location', 'summary', 'description'];
+const qs = require('querystring'),
+      util = require('../util');
 
 /*
- * Proxy call to Google calendar events to return the next event
+ * Proxy call to the Eventbrite API to return the next event
  */
 
 class CalendarHandler {
     constructor(config) {
-        this.calId = config.GOOGLE_CAL_ID;
-        this.host = 'www.googleapis.com';
-        this.path = '/calendar/v3/calendars/' + this.calId + '/events';
-        this.apiKey = config.GOOGLE_API_KEY;
+        this.page = config.EVENTBRITE_PAGE;
+        this.organizer_id = config.EVENTBRITE_ORGANIZER;
+        this.host = 'www.eventbriteapi.com';
+        this.path = '/v3/events/search/';
+        this.token = config.EVENTBRITE_TOKEN;
     }
 
     handle(req, context) {
         const query = qs.stringify({
-            orderBy: 'startTime',
-            singleEvents: 'True',
-            maxResults: '20',
-            timeMin: new Date().toISOString(),
-            key: this.apiKey
+            'sort_by': 'date',
+            'organizer.id': this.organizer_id,
+            'token': this.token
         });
 
-        const params = {
-            host: this.host,
-            path: this.path + '?' + query,
-            headers: {
-                'Accept': 'application/json'
+        util.req(this.host, this.path + '?' + query, (err, resp) => {
+            if (err) return context.done('ERR_INTERNAL_ERROR');
+            let ev = {page: this.page};
+            if (!resp.events[0]) {
+                return context.done(null, {data: ev});
             }
-        };
 
-        let r = https.request(params, (res) => {
-            if (res.statusCode !== 200) {
-                return context.done('ERR_INTERNAL_ERROR');
-            }
-            let body = '';
-            res.on('data', (chunk) => {
-                body += chunk;
-            });
+            ev.summary = resp.events[0].name.text;
+            ev.url = resp.events[0].url;
+            ev.start = resp.events[0].start.utc;
 
-            res.on('end', () => {
-                let content = JSON.parse(body);
-                let ev = {calId: this.calId};
-                if (content.items[0]) {
-                    FIELDS.forEach((f) => {
-                        ev[f] = content.items[0][f];
-                    });
-                }
+            let venuePath = '/v3/venues/' + resp.events[0].venue_id +
+                            '/?token=' + this.token;
+            util.req(this.host, venuePath, (err, resp) => {
+                if (err) return context.done('ERR_INTERNAL_ERROR');
+                ev.location = resp.address.localized_address_display;
                 context.done(null, {data: ev});
             });
         });
-
-        r.on('error', () => context.done('ERR_INTERNAL_ERROR'));
-        r.end();
     }
 }
 
